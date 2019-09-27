0ba9dc03b0ddd81c43de95d4fab0d4b42c6ca76d
Ben Partch <ben@retronetro.com>
2019-09-27 05:50:14 -0500

    edit "about" page content

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,6 +10,9 @@ const AboutPage = ({ data }) => {
       <Content>
         <h1>About</h1>
         <p>Hi. I'm Ben, nice to meet you. 🤣 This is maybe going to be my website. Let's give it a few months and see what I can make it do. 💥</p>
+
+        <h2>What's going on with your site?</h2>
+        <p>I'm getting used to blogging; and I have nothing to blog about right now. So I'm just collecting stuff "notes" to share with my GF. 😘💖</p>
       </Content>
     </Container>
   )
